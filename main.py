af06eac2f4ebfa40ba36fdc0ff8066f691d76b05
Adam Ben Shoham <mada20942@gmail.com>
2026-01-21 09:38:58 +0200

    feat: add list of chars to clean, create instance of observer and handler in main run

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
 import time
+from pathlib import Path
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+chars_to_remove = ['a','b','c','d','e',
+                   'f','g','h','i','j',
+                   'k','l','m','n','o',
+                   'p','q','r','s','t',
+                   'u','v','w','x','y',
+                   'z','(',')','*','@',
+                   '&','{','}','^']
+
 class MyHandler(FileSystemEventHandler):
     def on_modified(self,event):
-        if not event.is_directory and event.src_path.endswith('.txt'):
-            self.read_file_content(event.src_path)
 
-    def read_file_content(self,file_path):
-        with open(file_path,'w',encoding='utf-8') as f:
-            content = f.read()
-            print(content)
+        if not event.is_directory and event.src_path.endswith('.txt'):
+
+            time.sleep(0.1)
+            with open(event.src_path, 'r', encoding='utf-8') as f:
+                text = f.read().lower()
+
+
+
 
     def on_created(self,event):
         pass
+
+if __name__ == "__main__":
+
+    event_handler = MyHandler()
+
+    home = Path.home()
+    test_folder = home / 'Downloads' / 'test'
+    observer = Observer()
+    observer.schedule(event_handler, path=test_folder, recursive=False)
+    observer.start()
+
+    try:
+        while observer.is_alive():
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        observer.stop()
+
+    except Exception as e:
+        observer.stop()
+
+    observer.join()
+
+
+
 
 
