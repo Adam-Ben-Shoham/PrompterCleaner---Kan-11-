718170c41b7a5c9086fce1e0f76d3686a39046c9
Adam Ben Shoham <mada20942@gmail.com>
2026-01-22 09:56:44 +0200

    feat: change the logic to watch the folder that the code is in

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import sys
 import time
 from pathlib import Path
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import os
+
+if getattr(sys, 'frozen', False):
+    current_dir = Path(sys.executable).parent.resolve()
+else:
+    current_dir = Path(__file__).parent.resolve()
 
 chars_to_remove = 'abcdefghijklmnopqrstuvwxyz()*@&{}^'
 niqqud_chars = ''.join(chr(c) for c in range(0x05B0, 0x05C8))
@@ -9,50 +16,68 @@ complete_removal_list = chars_to_remove + niqqud_chars
 
 table = str.maketrans('', '', complete_removal_list)
 
+
 class MyHandler(FileSystemEventHandler):
 
     def clean_text(self, file_path):
-        with open(file_path, 'r', encoding='utf-8') as f:
-            text = f.read().lower()
-            cleaned_text = text.translate(table)
 
-        if text == cleaned_text:
+        try:
+
+            with open(file_path, 'r', encoding='utf-8') as f:
+                text = f.read().lower()
+                cleaned_text = text.translate(table)
+
+            if text == cleaned_text:
+                return
+
+            temp_file = file_path + '.tmp'
+
+            with open(temp_file, 'w', encoding='utf-8') as f:
+                f.write(cleaned_text)
+
+            os.replace(temp_file, file_path)
+
+            log_action(f'Successfully cleaned {file_path}\n')
+
+        except FileNotFoundError:
+            log_action(f'{file_path} not found')
+        except PermissionError:
+            log_action(f'Permission Error for: {file_path} \n')
+        except Exception as e:
+            log_action(f'Unknown Error for: {file_path} \n')
+
+    def on_modified(self, event):
+
+        if 'cleaning_log.txt' in event.src_path or event.src_path.endswith('.tmp'):
             return
 
-        with open(file_path, 'w', encoding='utf-8') as f:
-            f.write(cleaned_text)
-
-    def on_modified(self,event):
-
-        if 'cleaning_log.txt' in event.src_path:
-            return
-
-
-        if not event.is_directory and event.src_path.endswith('ר.txt'):
-            log_action(f'Cleaning {event.src_path} \n')
-            time.sleep(0.5)
+        if not event.is_directory and event.src_path.lower().endswith('.txt'):
+            time.sleep(0.7)
             self.clean_text(event.src_path)
 
-
-
-    def on_created(self,event):
+    def on_created(self, event):
         pass
 
+
 def log_action(message):
-    log_path = home / 'Downloads' / 'test' / 'cleaning_log.txt'
+    log_path = current_dir / 'cleaning_log.txt'
+
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
-    with open(log_path, "a",encoding='utf-8') as f:
+
+    with open(log_path, "a", encoding='utf-8') as f:
         f.write(f"[{timestamp}] {message}\n")
         f.flush()
+
 
 if __name__ == "__main__":
 
     event_handler = MyHandler()
 
-    home = Path.home()
-    test_folder = home / 'Downloads' / 'test'
+    # home = Path.home()
+    # test_folder = home / 'Downloads' / 'test'
+
     observer = Observer()
-    observer.schedule(event_handler, path=test_folder, recursive=False)
+    observer.schedule(event_handler, path=str(current_dir), recursive=False)
     observer.start()
 
     try:
@@ -66,8 +91,3 @@ if __name__ == "__main__":
         observer.stop()
 
     observer.join()
-
-
-
-
-
